6d9f1b0e7479776fcbe2511976ee6d658c5daeea
jmateusousa <contato.matthewd@gmail.com>
2017-09-15 13:22:23 -0300

    up

---
+++ pages/servidores.php
@@ -0,0 +1,14 @@
+<?php
+
+// S -> servidor
+// R -> roteador
+// SW -> Switch
+// P -> impressora
+// DV -> DVR
+
+//SERVIDORES
+// ----------- IP : nome do servidor : Tipo de icone
+$servidor[] = "192.168.1.60:Portal :S"; 
+$servidor[] = "192.168.1.61:Aplicação:S"; 
+
+?>
